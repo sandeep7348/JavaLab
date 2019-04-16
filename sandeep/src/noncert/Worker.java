5813ebb037b840fa300315949f8113666dec63f3
500064341 <500064341@upes-500064341.DDN.UPES.AC.IN>
2019-04-16 09:22:25 +0530

    Practice

---
+++ sandeep/src/noncert/Worker.java
@@ -0,0 +1,22 @@
+package noncert;
+
+public class Worker {
+  public String name;
+  public int salary;
+ 
+  Worker(String empname, int income)
+  {
+	  name=empname;
+	  salary=income;
+	  
+  }
+  void computePay(int h)
+  {
+	  System.out.println("Name of the worker is "+ name);
+	  System.out.println("Name of the worker is "+ salary);
+	  		
+	  
+	  
+  }
+}
+
